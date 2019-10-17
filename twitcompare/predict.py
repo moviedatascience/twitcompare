293d98a0040e93f1ed5a8dd46a51567730129b7e
moviedatascience <moviedatascience@users.noreply.github.com>
2019-10-17 13:08:47 -0400

    fixed comparison function

--- twitcompare/predict.py
+++ twitcompare/predict.py
@@ -17,11 +17,12 @@ def predict_user(user1_name, user2_name, tweet_text):
     """
     user1 = User.query.filter(User.username == user1_name).one()
     user2 = User.query.filter(User.username == user2_name).one()
-    user1_embeddings = np.array([tweet.embedding for tweet in user1.tweet])
-    user2_embeddings = np.array([tweet.embedding for tweet in user2.tweet])
+    user1_embeddings = np.array([tweet.embedding for tweet in user1.tweets])
+    user2_embeddings = np.array([tweet.embedding for tweet in user2.tweets])
     embeddings = np.vstack([user1_embeddings, user2_embeddings])
-    labels = np.concatenate([np.ones(len(user1.tweet)),
-                             np.zeros(len(user2.tweet))])
+    labels = np.concatenate([np.ones(len(user1.tweets)),
+                             np.zeros(len(user2.tweets))])
     log_reg = LogisticRegression().fit(embeddings, labels)
     tweet_embedding = BASILICA.embed_sentence(tweet_text, model='twitter')
+    # import pdb; pdb.set_trace()
     return log_reg.predict(np.array(tweet_embedding).reshape(1, -1))
